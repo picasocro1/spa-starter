103d2ade8fc3242e1f2c92d7fc6b20f7147416f9
Maciej <kubiak@bryter.io>
2024-03-04 02:09:06 +0100

    use BASE_URL

--- src/main.tsx
+++ src/main.tsx
@@ -5,31 +5,34 @@ import { createBrowserRouter, Navigate, RouterProvider } from 'react-router-dom'
 import { About, ErrorPage, Home, Project, Root } from '@/routes'
 import './index.css'
 
-const router = createBrowserRouter([
-  {
-    path: '/',
-    element: <Root />,
-    errorElement: <ErrorPage />,
-    children: [
-      {
-        path: '/',
-        element: <Navigate to="/home" />,
-      },
-      {
-        path: '/home',
-        element: <Home />,
-      },
-      {
-        path: '/about',
-        element: <About />,
-      },
-      {
-        path: '/project/:id',
-        element: <Project />,
-      },
-    ],
-  },
-])
+const router = createBrowserRouter(
+  [
+    {
+      path: '/',
+      element: <Root />,
+      errorElement: <ErrorPage />,
+      children: [
+        {
+          path: '/',
+          element: <Navigate to="/home" />,
+        },
+        {
+          path: '/home',
+          element: <Home />,
+        },
+        {
+          path: '/about',
+          element: <About />,
+        },
+        {
+          path: '/project/:id',
+          element: <Project />,
+        },
+      ],
+    },
+  ],
+  { basename: import.meta.env.BASE_URL },
+)
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
